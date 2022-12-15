1863c66bde914941ba232d6b752785854fd6483a
seodoha <doha.seo89@gmail.com>
2022-12-15 10:40:44 +0900

    setup

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+    bgColor: "#3c40c6",
+    textColor: "white",
+    accentColor: "#9c88ff",
+    cardBgColor: "transparent",
+};
