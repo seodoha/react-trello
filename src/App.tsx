fb40bf8aabc59d7089dcb9d767a5ac75f9ce4247
서도하\user <doha.seo89@gmail.com>
2023-10-19 21:38:43 +0900

    555

--- src/App.tsx
+++ src/App.tsx
@@ -77,6 +77,7 @@ function App() {
                     {Object.keys(toDos).map((boardId: string) => (
                         <Board toDos={toDos[boardId]} key={boardId} boardId={boardId} />
                     ))}
+                    1231231231231231231231
                     <Trash />
                 </Boards>
             </Wrapper>
